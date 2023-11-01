97f725cf4581b3d074bbbc585a9e6bff78a6ce83
SemahChaouch <chaouchmohamedsameh@gmail.com>
2023-11-01 20:41:59 +0100

    added justify's logic

---
+++ src/utils/justify.utils.ts
@@ -0,0 +1,61 @@
+function addSpace(sentence: string): string {
+    let i = 0;
+    while (sentence.length < 80 && i < 80) {
+        if (sentence.length < 40) {
+            break;
+        }
+        if (sentence[i] === ' ') {
+            sentence = sentence.slice(0, i) + ' ' + sentence.slice(i);
+            i += 2;
+        } else {
+            i++;
+        }
+        if (i >= sentence.length) {
+            i = 0;
+        }
+    }
+    return sentence;
+  }
+  
+  function splitTextIntoParagraphs(text: string): string[] {
+    const paragraphs: string[] = text.split(/\n{2,}/);
+    return paragraphs;
+  }
+  
+  function splitTextIntoSentences(text: string, maxCharacters: number): string[] {
+    const words: string[] = text.split(' ');
+    let currentSentence: string = '';
+    const sentences: string[] = [];
+  
+    for (const word of words) {
+        if (!currentSentence) {
+            currentSentence = word;
+        } else {
+            const potentialSentence: string = `${currentSentence} ${word}`;
+            if (potentialSentence.length <= maxCharacters) {
+                currentSentence = potentialSentence;
+            } else {
+                sentences.push(currentSentence);
+                currentSentence = word;
+            }
+        }
+    }
+  
+    if (currentSentence) {
+        sentences.push(currentSentence);
+    }
+  
+    return sentences;
+  }
+  const justifyText = ( text: string ): string => {
+    let result: string = '';
+    const maxCharactersPerSentence = 80;
+    const paragraphs= splitTextIntoParagraphs(text);
+    for (const paragraph of paragraphs) {
+    const sentences = splitTextIntoSentences(paragraph, maxCharactersPerSentence);
+    for (const sentence of sentences) {
+      const justifiedSentence = addSpace(sentence);
+      result += justifiedSentence + '\n';
+    }};
+  return result;
+  }
